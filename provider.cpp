858a6c6b01e10b8c3821321d9078edf6eae681e3
Waryas <waryas.z@gmail.com>
2022-07-20 17:38:00 +0200

    More separation of cpp/h file, should be clean now

--- provider.cpp
+++ provider.cpp
@@ -9,6 +9,8 @@ extern const char* prototypedMsg = "\033[38;5;46mPrototyped\033[0m\n";
 extern const char* passthroughMsg = "\033[38;5;11mPassthrough\033[0m\n";
 extern const char* notimplementedMsg = "\033[38;5;9mNot Implemented\033[0m\n";
 
+extern const HMODULE ntdll = LoadLibraryA("ntdll.dll");
+
 uint64_t unimplemented_stub() {
 	Logger::Log("\t\t\033[38;5;9mINSIDE STUB, RETURNING 0\033[0m\n");
 	return 0;
--- provider.h
+++ provider.h
@@ -2,12 +2,14 @@
 
 #include <inttypes.h>
 
+
+
 #define STUB_UNIMPLEMENTED 1//If you define this, every non prototyped function will return 0 instead of exiting
 
 extern const char* prototypedMsg;
 extern const char* passthroughMsg;
 extern const char* notimplementedMsg;
-
+extern const HMODULE ntdll;
 uint64_t unimplemented_stub();
 
 
